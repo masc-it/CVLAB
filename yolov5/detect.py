6e25b71d7148edd70fff2870ed95071cd053c524
onipot <m.sciancalepore20@studenti.uniba.it>
2022-02-19 13:01:13 +0100

    speed up app startup: move yolo imports into detect()

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -1,28 +1,5 @@
 # YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-"""
-Run inference on images, videos, directories, streams, etc.
 
-Usage - sources:
-    $ python path/to/detect.py --weights yolov5s.pt --source 0              # webcam
-                                                             img.jpg        # image
-                                                             vid.mp4        # video
-                                                             path/          # directory
-                                                             path/*.jpg     # glob
-                                                             'https://youtu.be/Zgi9g1ksQHc'  # YouTube
-                                                             'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
-
-Usage - formats:
-    $ python path/to/detect.py --weights yolov5s.pt                 # PyTorch
-                                         yolov5s.torchscript        # TorchScript
-                                         yolov5s.onnx               # ONNX Runtime or OpenCV DNN with --dnn
-                                         yolov5s.xml                # OpenVINO
-                                         yolov5s.engine             # TensorRT
-                                         yolov5s.mlmodel            # CoreML (MacOS-only)
-                                         yolov5s_saved_model        # TensorFlow SavedModel
-                                         yolov5s.pb                 # TensorFlow GraphDef
-                                         yolov5s.tflite             # TensorFlow Lite
-                                         yolov5s_edgetpu.tflite     # TensorFlow Edge TPU
-"""
 import os
 import sys
 from pathlib import Path
@@ -31,30 +8,9 @@ FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
-#print(str(ROOT))
-sys.path.append(str(ROOT) + "/../../..")
-""" sys.path.append(str(ROOT) + "/utils")
-sys.path.append(str(ROOT) + "/models") """
 
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
-from utils.torch_utils import select_device, time_sync
-from utils.plots import Annotator, colors, save_one_box
-from utils.general import (check_img_size,
-                           increment_path, non_max_suppression, print_args, scale_coords, xyxy2xywh)
-from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages
-from models.common import DetectMultiBackend
 
-import argparse
-
-import cv2
-import torch
-
-id_to_class = {
-    0: "TITLE",
-    1: "P",
-    2: "LI"
-}
-@torch.no_grad()
 def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
@@ -82,6 +38,15 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
         ):
+    
+    import cv2
+    import torch
+    from utils.torch_utils import select_device, time_sync
+    from utils.plots import Annotator, colors, save_one_box
+    from utils.general import (check_img_size,
+                            increment_path, non_max_suppression, print_args, scale_coords, xyxy2xywh)
+    from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages
+    from models.common import DetectMultiBackend
     source = str(source)
 
     save_img = not nosave and not source.endswith('.txt')  # save inference images
@@ -167,7 +132,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
                 with open(txt_path + '.txt', 'w') as f:
                     for *xyxy, conf, cls in reversed(det):
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        preds.append({"class": id_to_class[int(cls)], "left": xywh[0], "top": xywh[1], "w": xywh[2],"h": xywh[3], "conf": float(conf)})
+                        preds.append({"class": int(cls), "left": xywh[0], "top": xywh[1], "w": xywh[2],"h": xywh[3], "conf": float(conf)})
                         if save_txt:  # Write to file
                             line = (int(cls), *xywh, conf) if save_conf else (cls, *xywh)  # label format
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
@@ -215,49 +180,3 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
     if update:
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning) """
-
-
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
-    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    print_args(FILE.stem, opt)
-    return opt
-
-
-def main(opt):
-    #check_requirements(exclude=('tensorboard', 'thop'))
-    for i in run(**vars(opt)):
-        pass
-
-
-if __name__ == "__main__":
-    opt = parse_opt()
-    main(opt)
-        
